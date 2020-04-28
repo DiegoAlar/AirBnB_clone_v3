36cb1ab0d08deac00ddcd34f667da9e9ae95a6f9
dagute <1271@holbertonschool.com>
2020-04-28 23:54:10 +0000

    state

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,6 +8,7 @@ from api.v1.views import app_views
 API_HOST = getenv('HBNB_API_HOST')
 API_PORT = getenv('HBNB_API_PORT')
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
