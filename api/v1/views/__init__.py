1c8c75dc57b3914ed4fd9fa88d09230ccc1e7252
dagute <1271@holbertonschool.com>
2020-04-28 22:34:50 +0000

    state

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,8 +4,9 @@
 
 
 from flask import Flask, jsonify, Blueprint
-from api.v1.views.index import *
-import api.v1.views.states
 
 
 app_views = Blueprint('app_views', __name__)
+from api.v1.views.index import *
+from api.v1.views.states import *
+
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,17 +2,33 @@
 from os import getenv
 from flask import Flask, jsonify, Blueprint, make_response
 from models import storage
-from api.v1.views.__init__ import app_views
-from api.v1.app import app
+from api.v1.views import app_views
+from api.v1 import app
 
 @app.route('/states', methods=['GET', 'POST'])
-@app.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT')
-def state_objs(state_id=None):
-    """Retrieves all states or and specific state"""
+@app.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
+def states_routes(state_id=None):
+    objs = storage.all('State')
     if state_id:
-        key = "state" + id
+        key = "State." + id
         obj = objs.get(key)
-        if request.method == 'GET'
-           if obj:
-           return json(obj)
-           else:
+        if request.method == 'GET':
+            if obj:
+                return jsonify(obj)
+            else:
+                abort(404)
+        elif request.method == 'DELETE':
+            if obj:
+                storage.delete(obj)
+                return make_response(jsonify({}), 200)
+            else:
+                abort(404)
+        elif request.method == 'POST':
+            pass
+        elif request.method == 'PUT':
+            pass
+    else:
+        states_list = []
+        for obj in objs.values():
+            states_list.append(obj.to_dict())
+        return jsonify(states_list)
