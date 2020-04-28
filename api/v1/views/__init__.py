c8fa779560e6123361746455d3d6b9ea403744c0
dagute <1271@holbertonschool.com>
2020-04-28 23:54:56 +0000

    state

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,4 +9,3 @@ from flask import Flask, jsonify, Blueprint
 app_views = Blueprint('app_views', __name__)
 from api.v1.views.index import *
 from api.v1.views.states import *
-
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
 from os import getenv
-from flask import Flask, jsonify, Blueprint, make_response
+from flask import Flask, jsonify, Blueprint, make_response, request, abort
 from models import storage
 from api.v1.views import app_views
 from api.v1 import app
+from models.state import State
 
-@app.route('/states', methods=['GET', 'POST'])
-@app.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
+@app_views.route('/states', strict_slashes=False, methods=['GET', 'POST'])
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET', 'DELETE', 'PUT'])
 def states_routes(state_id=None):
     objs = storage.all('State')
     if state_id:
-        key = "State." + id
+        key = "State." + state_id
         obj = objs.get(key)
+        if obj is None:
+            abort(404)
         if request.method == 'GET':
-            if obj:
-                return jsonify(obj)
-            else:
-                abort(404)
+            return jsonify(obj.to_dict())
         elif request.method == 'DELETE':
-            if obj:
-                storage.delete(obj)
-                return make_response(jsonify({}), 200)
-            else:
-                abort(404)
-        elif request.method == 'POST':
-            pass
+            storage.delete(obj)
+            return make_response(jsonify({}), 200)
         elif request.method == 'PUT':
-            pass
+            if not request.get_json():
+                return ('Not a JSON'), 400
     else:
+        if request.method == 'POST':
+            if not request.get_json():
+                return ('Not a JSON'), 400
+            data = request.get_json()
+            if not data['name']:
+                return ('Missing name'), 400
+            new_state = State(data)
+            return (new_state.to_dict()), 201
         states_list = []
         for obj in objs.values():
             states_list.append(obj.to_dict())
