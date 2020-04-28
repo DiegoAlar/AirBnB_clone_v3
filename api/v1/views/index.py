5ae327afe71eb2bdb1d2b442168d59675a64330e
DiegoAlar <diegoaav@hotmail.com>
2020-04-28 10:38:08 -0500

    adding route /api/v1/stats (task 5)

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,11 +5,27 @@
 
 from api.v1.views.__init__ import app_views
 from flask import Flask, jsonify, Blueprint
+from models import storage
 
 
 @app_views.route('/status')
 def status():
+    """ show status """
     status = {
             "status": "OK"
         }
     return jsonify(status)
+
+
+@app_views.route('/stats')
+def stats():
+    """ Show stats """
+    stats = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+        }
+    return jsonify(stats)
