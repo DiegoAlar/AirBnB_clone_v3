2109563ba8abea532b2c29f0f46f4c9c0748cef3
dagute <1271@holbertonschool.com>
2020-04-29 21:25:58 +0000

    User

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -4,7 +4,7 @@ from flask import jsonify, make_response, request, abort
 from models import storage
 from api.v1.views import app_views
 from models.state import State
-from models.user import Users
+from models.user import User
 from models.base_model import BaseModel
 
 
