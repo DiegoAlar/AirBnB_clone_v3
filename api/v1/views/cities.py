532e3c8ea0e77300de99d88f870dc362f22fb271
DiegoAlar <diegoaav@hotmail.com>
2020-04-29 13:50:38 -0500

    adding cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -13,13 +13,12 @@ mets = ['GET', 'POST']
 mets_id = ['GET', 'DELETE', 'PUT']
 
 
-@app_views.route('/states', strict_slashes=False, methods=mets)
-@app_views.route('/states/<state_id>', strict_slashes=False, methods=mets_id)
-def states_routes(state_id=None):
-    """States_routes"""
-    objs = storage.all('State')
-    if state_id:
-        key = "State." + state_id
+@app_views.route('/cities/<city_id>', strict_slashes=False, methods=mets_id)
+def cities_routes(city_id=None):
+    """ Cities_routes """
+    objs = storage.all('City')
+    if city_id:
+        key = "City." + city_id
         obj = objs.get(key)
         if obj is None:
             abort(404)
@@ -27,6 +26,7 @@ def states_routes(state_id=None):
             return jsonify(obj.to_dict())
         elif request.method == 'DELETE':
             storage.delete(obj)
+            storage.save()
             return make_response(jsonify({}), 200)
         elif request.method == 'PUT':
             if not request.get_json():
@@ -51,3 +51,30 @@ def states_routes(state_id=None):
         for obj in objs.values():
             cities_list.append(obj.to_dict())
         return jsonify(cities_list)
+
+@app_views.route('/states/<state_id>/cities', strict_slashes=False, methods=mets)
+def cities_routes2(state_id=None):
+    """ return city objects """
+    objs = storage.all('State')
+    if state_id:
+        key = "State." + state_id
+        obj = objs.get(key)
+        if obj is None:
+            abort(404)
+        if request.method == 'GET':
+            cities_list = []
+            for obj in objs.values():
+                if obj.state_id == state_id:
+                    cities_list.append(obj.to_dict())
+            return jsonify(cities_list)
+        elif request.method == 'POST':
+            if not request.get_json():
+                return ('Not a JSON'), 400
+            data = request.get_json()
+            if not data['name']:
+                return ('Missing name'), 400
+            data['state_id'] = state_id
+            new_city = City(**data)
+            storage.new(new_city)
+            storage.save()
+            return make_response(jsonify(new_city.to_dict()), 201)
