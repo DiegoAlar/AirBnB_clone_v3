af852f7a73bf473ef91d2fc35a107288b3d0a49d
dagute <1271@holbertonschool.com>
2020-04-30 07:03:40 +0000

    fix comments

---
+++ api/v1/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Status of your API"""
+from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
+from models import storage
+from os import getenv
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def teardown_app_context(self):
+    """teardown"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ not found """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    port = '5000'
+    if getenv('HBNB_API_HOST'):
+        host = getenv('HBNB_API_HOST')
+    if getenv('HBNB_API_PORT'):
+        port = getenv('HBNB_API_PORT')
+    app.run(host=host, port=port, threaded=True)
